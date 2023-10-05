9db8178be01e2cd3f17442015f3b6740dcba44cc
Charu-2718 <charu.15421@gmail.com>
2023-10-05 22:36:58 +0530

    New

---
+++ Problems/Sqrt(x).cpp
@@ -0,0 +1,15 @@
+
+class Solution {
+public:
+    int mySqrt(int x) {
+        if (x == 0 || x == 1)
+            return x;
+        long i = 1, result = 1;
+    while (result <= x) {
+        i++;
+        result = i * i;
+    }
+    return i - 1;
+
+    }
+};
