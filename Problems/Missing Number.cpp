fcad9ca34709fcefa105d65c8de04acc5514b918
Charu-2718 <charu.15421@gmail.com>
2023-10-09 23:31:02 +0530

    New

--- Problems/Missing Number.cpp
+++ Problems/Missing Number.cpp
@@ -11,3 +11,41 @@ public:
         return totalSum;
     }
 };
+
+
+//Another method
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int n = nums.size();
+        int maxi = INT_MIN;
+        
+        for(int i = 0; i < n; i++) {
+            maxi = max(maxi, nums[i]);
+        }
+        
+        int ans = -1; 
+
+        for(int j = 0; j <= maxi; j++) {
+            bool found = false; 
+            
+            for(int i = 0; i < n; i++) {
+                if(nums[i] == j) {
+                    found = true;
+                    break; 
+                }
+            }
+            
+            if (!found) {
+                ans = j; 
+            }
+        }
+
+        if (ans == -1) {
+            ans = n;
+        }
+
+        return ans;
+    }
+};
+
