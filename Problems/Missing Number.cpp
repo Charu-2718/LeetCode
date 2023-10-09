88dc56edc44bce65ac01c48f7a0802d0b4ebe843
Charu-2718 <charu.15421@gmail.com>
2023-10-09 23:30:08 +0530

    New

---
+++ Problems/Missing Number.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int n = nums.size();
+        int totalSum = n * (n + 1) / 2; 
+        
+        for (int i = 0; i < n; i++) {
+            totalSum -= nums[i]; 
+        }
+        
+        return totalSum;
+    }
+};
