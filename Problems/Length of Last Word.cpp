48073b8fd3fb8eda89f722d9e67cb055d6591c43
Charu-2718 <charu.15421@gmail.com>
2024-01-05 13:51:25 +0530

    new

---
+++ Problems/Length of Last Word.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+        int n = s.size();
+        int ans = 0 ;
+        int flag = 0;
+        for(int i=n-1;i>=0;i--){
+            if(s[i]==' '&&flag)break;
+            if(s[i]!=' '){
+                flag=1;
+                ans++;
+            }
+        }
+        return ans;
+    }
+};
