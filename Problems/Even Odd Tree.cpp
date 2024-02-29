619c0a7af5b8a40db11871175182de0668d5acdc
Charu-2718 <charu.15421@gmail.com>
2024-02-29 23:14:39 +0530

    new

---
+++ Problems/Even Odd Tree.cpp
@@ -0,0 +1,50 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool isEvenOddTree(TreeNode* root) {
+        if (root == nullptr)
+            return false; 
+
+        queue<TreeNode*> q;
+        q.push(root);
+        int level = -1; 
+        while (!q.empty()) {
+            level++;
+            int n = q.size();
+            int prev ;
+            if(level % 2 == 0)  {
+                prev=INT_MIN;
+            } 
+            else {
+                prev=INT_MAX;
+            }
+            for (int i = 0; i < n; i++) {
+                TreeNode* ptr = q.front();
+                q.pop();
+                int val = ptr->val;
+                if ((level % 2 == 0 && (val % 2 == 0 || val <= prev)) || 
+                    (level % 2 != 0 && (val % 2 != 0 || val >= prev))) {
+                    return false;
+                }
+                prev = val;
+                if (ptr->left) {
+                    q.push(ptr->left);
+                }
+                if (ptr->right) {
+                    q.push(ptr->right);
+                }
+            }
+        }
+        return true;
+    }
+};
