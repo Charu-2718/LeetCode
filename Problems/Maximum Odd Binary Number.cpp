471d1f995eeb9e9bcbd1135dd4a6a92e6fb59538
Charu-2718 <charu.15421@gmail.com>
2024-03-01 21:13:38 +0530

    new

---
+++ Problems/Maximum Odd Binary Number.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    string maximumOddBinaryNumber(string s) {
+    //    int n = s.size();
+    //    string result = string(n,'0') ;
+    //    int i = 0; 
+    //    for(auto &ch: s){
+           
+    //         if (ch=='1'){
+
+    //             if(result[n-1]=='1'){
+    //                 result[i]='1';
+    //                 i++;
+    //             }
+    //             else{
+    //                 result[n-1] = '1';
+    //             }
+    //        }
+    //    }
+    //    return result;
+       int n = s.size();
+       string result = string(n,'0') ;
+       int count_1 = count(s.begin(),s.end(),'1');
+
+       int j = 0;
+        
+       while(count_1 > 1) {
+            result[j] = '1';
+            j++;
+            
+            count_1--;
+        }
+        
+        result[n-1] = '1';
+        return result;
+    }
+};
